ddc1269633b02e421fa6091b1c2a5a1d9406482f
ChangePool <doc@coincashew.io>
2026-02-11 02:07:00 -0800

    Edit baseUrl option

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,8 @@ const config = {
   url:  'https://coincashew.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  //baseUrl: '/',
+  baseUrl: '/spo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
